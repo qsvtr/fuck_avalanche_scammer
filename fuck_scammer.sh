161b702edba695de61c4888653edb04fb8fb6089
Quentin SAUVETRE <quentin.sauvetre@protonmail.com>
2021-02-27 16:51:24 +0100

    update script

--- fuck_scammer.sh
+++ fuck_scammer.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+#download wordlist
+if [ ! -f wordlist.txt ]; then wget -q -O wordlist.txt https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt; fi
+
 while true
 do
 	curl -o nul -sX POST -H "Content-Type: application/json" -d seed='{"seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'", "seed%5B5D:"'$(shuf -n 1 wordlist.txt)'"}"' https://api-services.xyz/avaX/step_seed.php?login=seed
-	echo "step 1: done"
 	sleep 2
 	rnd_privatekey="PrivateKey-"$(tr -dc A-Za-z0-9 </dev/urandom | head -c 50)
 	curl -o nul -sX POST -H "Content-Type: application/json" -d '{"seed":"'$rnd_privatekey'"}' https://api-services.xyz/avaX/step_key.php?login=key
-	echo "step2: done"
-	echo "repeat afer me: FUCK SCAMMERS"
+	echo "done - repeat afer me: FUCK SCAMMERS"
 	sleep 2
+
 done
